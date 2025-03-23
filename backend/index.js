33826a2b55907338feeaa3c4f47f5364e31c4aba
Daktar <kenntrixwambuzz@gmail.com>
2025-03-23 16:27:16 +0300

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,12 @@ import userRoutes from "./routes/user.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import orderRoutes from "./routes/order.routes.js";
-<<<<<<< HEAD
-import paymentRoutes from "./routes/payment.routes.js";
-=======
+// <<<<<<< HEAD
+// import paymentRoutes from "./routes/payment.routes.js";
+// =======
 
-// Load environment variables at the beginning
->>>>>>> 37fb385 (Update changes)
+// // Load environment variables at the beginning
+// >>>>>>> 37fb385 (Update changes)
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -52,7 +52,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/payments", paymentRoutes);
+// app.use("/api/payments", paymentRoutes);
 
 // Middleware for handling errors
 app.use((error, request, response, next) => {
